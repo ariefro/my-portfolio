5a61459d648773821c21ab68ddda895e4f4702e5
ariefro <ariefromadhon26@gmail.com>
2023-08-25 05:51:57 +0700

    add project description component

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -52,4 +52,16 @@ const Linkedin = () => (
   </svg>
 );
 
-export default { Close, Github, Hamburger, Linkedin };
+const Arrow = ({ className }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 20 20"
+  >
+    <path fill="currentColor" d="M11 18H9V6l-4 4l-2-1l7-7l7 7l-2 1l-4-4v12z" />
+  </svg>
+);
+
+export default { Arrow, Close, Github, Hamburger, Linkedin };
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Icon } from "./Icon";
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
+export { default as ProjectDescription } from "./ProjectDescription";
 export { default as Sidebar } from "./Sidebar";

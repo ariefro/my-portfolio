5e4d13d035d5e2fad1cfa1071f63f1bd59d7edef
ariefro <ariefromadhon26@gmail.com>
2023-08-28 20:00:56 +0700

    add layout for contact page

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -1,8 +1,5 @@
 import { Footer, Navbar, Sidebar } from "@/components";
 import "../../styles/globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ariefro | ABOUT",
--- src/app/contact/layout.jsx
+++ src/app/contact/layout.jsx
@@ -1,21 +1,17 @@
-import { Footer, Navbar, Sidebar } from "@/components";
+import { Navbar } from "@/components";
 import "../../styles/globals.css";
 
 export const metadata = {
-  title: "ariefro | WORK",
+  title: "ariefro | CONTACT",
   description:
     "Arief Romadhon is a backend developer who designs, builds, and maintains the server-side components of software applications.",
 };
 
-export default function ProjectsLayout({ children }) {
+export default function ContactLayout({ children }) {
   return (
-    <div className="bg-primary px-4">
+    <div className="bg-primary h-screen mx-auto px-4 text">
       <Navbar className="text-tertiary" bgLineColor="bg-tertiary" />
       {children}
-      <Footer
-        className="bg-tertiary text-primary"
-        buttonClassName="bg-primary text-tertiary"
-      />
     </div>
   );
 }

78487df65fedc69ee4c6528a17ed62edbe614075
ariefro <ariefromadhon26@gmail.com>
2023-08-28 17:35:26 +0700

    add input component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as Icon } from "./Icon";
 export { default as Footer } from "./Footer";
+export { default as Icon } from "./Icon";
+export { default as Input } from "./Input";
 export { default as Navbar } from "./Navbar";
 export { default as ProjectDescription } from "./ProjectDescription";
 export { default as Sidebar } from "./Sidebar";
